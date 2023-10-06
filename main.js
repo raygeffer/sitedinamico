c0623e898393582e00ee6e0009f77ca9e323126a
raygeffer <126877809+raygeffer@users.noreply.github.com>
2023-10-06 10:28:11 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contad)
